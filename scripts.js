56a1c3a3371efdfc71c7f20907f233a16c791504
salim-aydin <salimaydin340@gmail.com>
2023-05-09 16:58:13 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// her şeyi açıklayacağım yorum satırlarıyla
+document.querySelector(".hamburger-menu")
+.addEventListener("click", function() {
+   // hamburger menuye tıkladığımızda
+
+document.querySelector(".menu-items")
+.classList.toggle("active");  // menu-items classına active classını ekliyoruz
+  
+}
+);
+  
